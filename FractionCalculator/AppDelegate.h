14babf9c1fdde142e0e27e3f544afe565b71bc01
rolfcasula <rolf.casula@gmail.com>
2017-11-28 13:33:15 +0100

    Added a new comment

--- FractionCalculator/AppDelegate.h
+++ FractionCalculator/AppDelegate.h
@@ -6,6 +6,8 @@
 //  Copyright © 2015 Rolf Alexandro Casula. All rights reserved.
 //
 
+// The latest update I made to this app was with Xcode 7.3 (Swift 2.2), therefore a more recent version of Xcode may not work properly
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
